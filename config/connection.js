8ca6fd300133bb6ade752360a1b5cdbe84d92f73
Faiyaz Rahman <faiyazrahman03@gmail.com>
2022-03-23 21:54:47 +0600

    Configured JAWDB

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,18 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-   process.env.DATABASE_NAME,
-   process.env.DATABASE_USERNAME,
-   process.env.DATABASE_PASSWORD,
-   {
+let sequelize;
+
+if (process.env.JAWDB_DATABASE_URL) {
+   console.log('Connected to JAWDB');
+   sequelize = new Sequelize(process.env.JAWDB_DATABASE_URL);
+} else {
+   console.log('Connected to localhost');
+   sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-   }
-);
+   });
+}
 
 module.exports = sequelize;
